878db7013be3f5787e919b528030af9c165bc123
aadil <aadil.psn@gmail.com>
2025-08-11 03:01:18 +0530

    Add files to upload

--- Minimizing Coins.cpp
+++ Minimizing Coins.cpp
@@ -1,79 +1,67 @@
+// Problem: Minimizing Coins
+// Link: https://cses.fi/problemset/task/1634/
+// Thought process: I recognized this as a classic unbounded knapsack problem, 
+// where we need to find the minimum number of coins to make a sum `w` from given denominations. 
+// I used an iterative dynamic programming approach with a 2-row rolling array to optimize memory usage.
+
 #include <bits/stdc++.h>
 using namespace std;
-#define mod 1000000007
-#define int long long
-#define double long double
-#define pb push_back
-#define eb emplace_back
-#define endl '\n'
-#define pii pair<int,int>
-#define min3(a,b,c) min(a,min(b,c))
-#define max3(a,b,c) max(a,max(b,c))
-#define all(x) x.begin(),x.end()
-#define fill(a,b) memset(a,b,sizeof(a))
-#define sz(x) (int)x.size()
-#define sp(x) setprecision(x)
-#define fi first
-#define se second
-#define lb lower_bound
-#define ub upper_bound
-#define bs binary_search
-#define inf 1000000000000000005LL
- 
- 
-int ceil(int a, int b) { // calculates ceil(a, b)
-    return (a+b-1)/b;
-}
- 
-int max(int a, int b) { //return max(a, b)
-    return a>b ? a : b;
-}
- 
-int binexp(int x, int n) { //return (x^n)%mod 
-    int ans = 1;
-    while(n>0) {
-        if(n%2)
-            ans = (ans*x)%mod;
-        x = (x*x)%mod;
-        n /= 2;
-    }
-    return ans;
-}
- 
-int arr[102];
-int dp[2][1000002];
-int mn = INT_MAX;
- 
-void solve() {
-    int n, w; cin >> n >> w;
-    for(int i = 0 ; i < n ; i++)
-        cin >> arr[i];
- 
-    for(int i = 0 ; i < 2; i++){
-        for(int j = 0 ; j < 1000002; j++){
-            dp[i][j] = mn;
-        }
-    }
-    for(int j = 1 ; j<=w ; j++)
-        dp[0][j] = mn;
- 
-    for(int i = 0 ; i < 2; i++)
-        dp[i][0] = 0;
- 
-    for(int i = 0 ; i < n; i++){
-        for(int j = 0; j <= w; j++){
-            if(arr[i] <= j){
-                dp[i%2][j] = min(1 + dp[i%2][j-arr[i]], dp[1-i%2][j]);
-            }
-            else {
-                dp[i%2][j] = dp[1-i%2][j];
+
+#define int long long // Use long long to handle large values safely
+
+class Solution {
+public:
+    int n, w;          // Number of coin types and target sum
+    int arr[102];      // Array to store coin denominations
+
+    // Core iterative DP function
+    int iterative_f() {
+        // dp[2][w+1]: Rolling array to reduce memory usage from O(n*w) to O(2*w)
+        // dp[i%2][j] = min coins to make sum j using first i coins
+        int dp[2][w + 1];
+
+        // Initialize all states to "infinity" (unreachable)
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j <= w; j++) {
+                dp[i][j] = INT_MAX;
             }
         }
+
+        // Base case: 0 coins needed to make sum 0
+        dp[0][0] = dp[1][0] = 0;
+
+        // Process each coin type
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j <= w; j++) {
+                // If we can use this coin without going negative in sum
+                if (j - arr[i] >= 0) {
+                    dp[i % 2][j] = 1 + dp[i % 2][j - arr[i]]; // Take the coin
+                }
+                // Always take the better option: use coin or skip it
+                dp[i % 2][j] = min(dp[i % 2][j], dp[1 - i % 2][j]);
+            }
+        }
+
+        // Return result or -1 if unreachable
+        return dp[1 - n % 2][w] == INT_MAX ? -1 : dp[1 - n % 2][w];
     }
-    int ans = dp[1-n%2][w];
-    cout << (ans==mn?-1:ans);
-}
- 
+
+    void solve() {
+        // Read inputs
+        cin >> n >> w;
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+
+        // Output result
+        cout << iterative_f();
+    }
+};
+
 signed main() {
-    solve();
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    Solution sol;
+    sol.solve();
 }
