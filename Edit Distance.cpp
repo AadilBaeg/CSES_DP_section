8c23b7c560ea238f09d28bcf00399b0d59a68fb3
Mirza Mohd Aadil Beg <50120271+AadilBaeg@users.noreply.github.com>
2025-08-16 05:29:07 +0530

    Update Edit Distance.cpp

--- Edit Distance.cpp
+++ Edit Distance.cpp
@@ -1,70 +1,57 @@
+// CSES Problem: Edit Distance
+// Link: https://cses.fi/problemset/task/1639/
+//
+// My Thought Process:
+// I recognized this as the classic edit distance (Levenshtein distance) problem.
+// My approach was to use recursion with memoization, where the state represents
+// the minimum operations needed to convert the first m characters of 'a' into
+// the first n characters of 'b'. At each step, I either match characters or
+// choose the cheapest edit (insert, delete, replace).
+
 #include <bits/stdc++.h>
 using namespace std;
-#define mod 1000000007
-#define int long long
-#define double long double
-#define pb push_back
-#define eb emplace_back
-#define endl '\n'
-#define pii pair<int,int>
-#define min3(a,b,c) min(a,min(b,c))
-#define max3(a,b,c) max(a,max(b,c))
-#define all(x) x.begin(),x.end()
-#define fill(a,b) memset(a,b,sizeof(a))
-#define sz(x) (int)x.size()
-#define sp(x) setprecision(x)
-#define fi first
-#define se second
-#define lb lower_bound
-#define ub upper_bound
-#define bs binary_search
-#define inf 1000000000000000005LL
- 
-int ceil(int a, int b) { // calculates ceil(a, b)
-    return (a+b-1)/b;
-}
- 
-int max(int a, int b) { //return max(a, b)
-    return a>b ? a : b;
-}
- 
-int binexp(int x, int n) { //return (x^n)%mod 
-    int ans = 1;
-    while(n>0) {
-        if(n%2)
-            ans = (ans*x)%mod;
-        x = (x*x)%mod;
-        n /= 2;
+
+class Solution {
+public:
+    string a, b;                 // Input strings
+    int dp[5002][5002];          // dp[m][n] = min operations to convert first m chars of a to first n chars of b
+
+    // Recursive DP function
+    int f(int m, int n) {
+        // Base case: if one string is empty, need to insert/delete all characters of the other
+        if (m == 0 || n == 0) return max(m, n);
+
+        // Check memoized result
+        int &ans = dp[m][n];
+        if (ans != -1) return ans;
+
+        if (a[m - 1] == b[n - 1]) {
+            // Characters match → no operation needed here
+            ans = f(m - 1, n - 1);
+        } else {
+            // Characters differ → take min of replace, insert, delete
+            ans = 1 + min({
+                f(m - 1, n - 1), // Replace
+                f(m, n - 1),     // Insert into 'a'
+                f(m - 1, n)      // Delete from 'a'
+            });
+        }
+        return ans;
     }
-    return ans;
-}
-int dp[5001][5001];
-string arr1, arr2;
-int f(int m, int n){
-    if(m==0 or n==0) return abs(m-n);
-    int &ans = dp[m][n];
-    if(ans != -1) return ans;
-    
-    ans = 1e5;
- 
-    if(arr1[m-1]==arr2[n-1]){
-        ans = f(m-1, n-1);
+
+    void solve() {
+        memset(dp, -1, sizeof dp);
+
+        cin >> a >> b;
+        int m = (int)a.size();
+        int n = (int)b.size();
+
+        int ans = f(m, n);
+        cout << ans;
     }
-    else{
-        ans = 1 + min3(f(m-1, n-1), f(m, n-1), f(m-1, n));
-    }
-    return ans;
-}
-void solve() {
-    memset(dp, -1, sizeof dp);
-    cin >> arr1;
-    cin >> arr2;
-    int m = (int)arr1.size();
-    int n = (int)arr2.size();
-    int ans = f(m, n);
-    cout << ans;
-}
- 
-signed main() {
-    solve();
+};
+
+int main() {
+    Solution s;
+    s.solve();
 }
